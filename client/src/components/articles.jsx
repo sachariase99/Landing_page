eb119eb0e859779b954ffc6f856eec2ce6657ca5
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-06-15 10:29:58 +0200

    Update articles.jsx

--- client/src/components/articles.jsx
+++ client/src/components/articles.jsx
@@ -59,7 +59,20 @@ const Articles = () => {
         <br />
         <p>Libero, quasi molestias. Obcaecati natus ad repudiandae a repellat. Porro distinctio minima qui sit error similique expedita possimus voluptate exercitationem quod accusamus numquam dicta earum rem, culpa quidem nihil pariatur.</p>
       </article>
-      
+      <article>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia iure voluptatem voluptatum a esse, blanditiis suscipit, quod minus dolore non iusto, ullam vitae culpa ex ducimus sequi sint iste aliquid!</p>
+        <br />
+        <p>Iste deserunt eaque, quaerat doloremque quisquam aperiam, voluptates deleniti itaque distinctio adipisci porro ratione earum ullam nulla omnis sint dolor perspiciatis libero tenetur totam rem nostrum, facilis impedit pariatur? Veniam?</p>
+        <br />
+        <p>Libero, quasi molestias. Obcaecati natus ad repudiandae a repellat. Porro distinctio minima qui sit error similique expedita possimus voluptate exercitationem quod accusamus numquam dicta earum rem, culpa quidem nihil pariatur.</p>
+      </article>
+      <article>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia iure voluptatem voluptatum a esse, blanditiis suscipit, quod minus dolore non iusto, ullam vitae culpa ex ducimus sequi sint iste aliquid!</p>
+        <br />
+        <p>Iste deserunt eaque, quaerat doloremque quisquam aperiam, voluptates deleniti itaque distinctio adipisci porro ratione earum ullam nulla omnis sint dolor perspiciatis libero tenetur totam rem nostrum, facilis impedit pariatur? Veniam?</p>
+        <br />
+        <p>Libero, quasi molestias. Obcaecati natus ad repudiandae a repellat. Porro distinctio minima qui sit error similique expedita possimus voluptate exercitationem quod accusamus numquam dicta earum rem, culpa quidem nihil pariatur.</p>
+      </article>
     </section>
   )
 }
