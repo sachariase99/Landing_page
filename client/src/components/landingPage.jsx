23578173a710711c38c1ed84e81e2e8b65c677f3
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-06-15 15:49:41 +0200

    Summary

--- client/src/components/landingPage.jsx
+++ client/src/components/landingPage.jsx
@@ -29,16 +29,16 @@ const LandingPage = () => {
         setIsInitialLoad(false);
         setTimeout(() => {
           setIsLoadingComplete(false);
-          setIsBlurEnabled(false); // Deaktiver sløring efter "Indlæsning fuldført!" forsvinder
-        }, 1000); // Fjern teksten "Indlæsning fuldført!" efter 1 sekund
-      }, 1000); // Vis "Indlæsning fuldført!" i 1 sekund
+          setIsBlurEnabled(false); // Disable blur after "Loading complete!" disappears
+        }, 1000); // Remove the "Loading complete!" text after 1 second
+      }, 1000); // Show "Loading complete!" for 1 second
     };
 
     window.addEventListener('load', handleLoad);
 
     return () => {
       clearInterval(timer);
-      window.removeEventListener('load', handleLoad); 
+      window.removeEventListener('load', handleLoad);
     };
   }, []);
 
@@ -51,11 +51,15 @@ const LandingPage = () => {
     <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div id="blurred" className="content">
         {isLoadingComplete ? (
-          <h1>Indlæsning fuldført!</h1>
+          <h1 className='uppercase font-bold text-6xl text-green-500 text-center'>Indlæsning fuldført!</h1>
         ) : (
           <div className={`loader ${isInitialLoad ? '' : 'hidden'}`}>
             <div className="loader-content">
-              <div className="percentage">{percentage}%</div>
+              <div className="progress-circle">
+                <div className="circle-border"></div>
+                <div className="circle-fill" style={{ clip: `rect(0px, ${percentage}px, 200px, 0px)` }}></div>
+                <div className="circle-content">{percentage}%</div>
+              </div>
             </div>
           </div>
         )}
